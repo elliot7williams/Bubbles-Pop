59a66d03212e572c1d2554f54aa0b7fb3c09b60d
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:24:15 -0400

    Add Bubbles Pop/Bubbles Pop/Bubbles_PopApp.swift

---
+++ Bubbles Pop/Bubbles Pop/Bubbles_PopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bubbles_PopApp.swift
+//  Bubbles Pop
+//
+//  Created by Elliot Williams on 2025-06-04.
+//
+
+import SwiftUI
+
+@main
+struct Bubbles_PopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
